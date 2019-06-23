7b440ee63e8e1f00ad6ef76a3d6e1cfbceb435f1
my name is jeff <oculusquestvr@gmail.com>
2019-06-23 15:32:30 -0600

    KAMguajshcKJasbcKs

--- session1/rps.py
+++ session1/rps.py
@@ -1,11 +1,7 @@
 p1=input('r,p,or s?')
 p2=input('r,p,or s?')
-if p1 and p2=='r'
-    print('ladies, ladies. your both pretty. actually your both ugly corpses.id rather date myself then you. play again')
-if p1 and p2=='p'
-    print('ladies, ladies. your both pretty. actually your both ugly corpses.id rather date myself then you. play again')
-if p1 and p2=='s'
-    print('ladies, ladies. your both pretty. actually your both ugly corpses.id rather date myself then you. play again')
+if p1 and p2=='r'or'p'or's':
+    print('tie')
 if p1=='r':
     #p1 wins
     if p2 == 's':
@@ -32,7 +28,30 @@ if p1=='i win':
         print('p1 wins automaticly. its super effective. p2 gets knocked out')
 if p2=='i win':
     #p2 wins
-        print('p1 dies in a hole p2 wins by default')
+        print('p1 dies in a hole. p2 wins by default')
 if p1 and p2=='i win':
     print('ladies, ladies. your both pretty. actually your both ugly corpses. play again')
-    
+if p1 and p2=='':
+    print('SAY SOMETHING!!!!!!!!!!!')
+if p1=='uno reverse card':
+    #p1 loses
+    if p2=='r'or'p'or's':
+        print('uno reverse failed')
+    if p2=='i win':
+        print('UNO REVERSE CARD! p1 wins') 
+if p2=='uno reverse card':
+    #p2 loses
+    if p1=='r'or'p'or's':
+        print('uno reverse failed')
+    if p1=='i win':
+        print('UNO REVERSE CARD! p2 wins') 
+if p1=='NOPE!!!':
+    if p2=='UNO REVERSE CARD!':
+        print('UNO and NOPE collide and create a world destroying reverse tornado')
+    if p2=='r'or'p'or's':
+        print('whoever thought p1 was genuis for choosing nope is an idiot along with p1. p2 wins obviously.')
+if p2=='NOPE!!!':
+    if p1=='UNO REVERSE CARD!':
+        print('UNO and NOPE collide and create a world destroying reverse tornado')
+    if p1=='r'or'p'or's':
+        print('whoever thought p2 was genuis for choosing nope card, is and idiot along with p2. p1 wins obviously.')
