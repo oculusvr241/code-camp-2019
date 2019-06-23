16cbe7b6bab9534c58cf7d3b5ec26fbea2ae2c07
my name is jeff <oculusquestvr@gmail.com>
2019-06-22 18:52:16 -0600

    ksudgksehkajdjkvkjJKDckJSDNkdjvjkNDEjkNDEJKVNjkdnv

--- session1/rps.py
+++ session1/rps.py
@@ -1,7 +1,11 @@
 p1=input('r,p,or s?')
 p2=input('r,p,or s?')
-if p1 == p2:
-    print('tie')
+if p1 and p2=='r'
+    print('ladies, ladies. your both pretty. actually your both ugly corpses.id rather date myself then you. play again')
+if p1 and p2=='p'
+    print('ladies, ladies. your both pretty. actually your both ugly corpses.id rather date myself then you. play again')
+if p1 and p2=='s'
+    print('ladies, ladies. your both pretty. actually your both ugly corpses.id rather date myself then you. play again')
 if p1=='r':
     #p1 wins
     if p2 == 's':
@@ -23,3 +27,12 @@ if p1=='s':
     #p1 loses
     if p2=='r':
         print('p2 wins with rock')
+if p1=='i win':
+    #p1 wins
+        print('p1 wins automaticly. its super effective. p2 gets knocked out')
+if p2=='i win':
+    #p2 wins
+        print('p1 dies in a hole p2 wins by default')
+if p1 and p2=='i win':
+    print('ladies, ladies. your both pretty. actually your both ugly corpses. play again')
+    
